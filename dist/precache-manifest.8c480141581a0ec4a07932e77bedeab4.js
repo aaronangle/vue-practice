2fea2da0b8bec1a512bbd0b2a77e732e1d819c44
Aaron Angle <acangle10@gmail.com>
2020-03-27 12:18:14 -0700

    add

---
+++ dist/precache-manifest.8c480141581a0ec4a07932e77bedeab4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b13f6c40613434bc00de",
+    "url": "/css/app.e76c111f.css"
+  },
+  {
+    "revision": "67384e17138e3181169aa0beb966891b",
+    "url": "/img/Capture-removebg-preview.67384e17.png"
+  },
+  {
+    "revision": "0a7507f72389962cc6d97e0a5da845ba",
+    "url": "/img/canvas.0a7507f7.png"
+  },
+  {
+    "revision": "6421d3cafdd2fe065ca33f12a37d7abf",
+    "url": "/img/monstermail.6421d3ca.png"
+  },
+  {
+    "revision": "bc144943f191f37be9872e6d374edf61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b13f6c40613434bc00de",
+    "url": "/js/app.01b81032.js"
+  },
+  {
+    "revision": "027cf12b82a27a6feb84",
+    "url": "/js/chunk-vendors.ba7fdb7b.js"
+  },
+  {
+    "revision": "bead8edfdfc12c88a60e7f8c85ce203d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
