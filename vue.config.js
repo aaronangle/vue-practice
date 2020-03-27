9d85e01e6509f7085d08f912d1c1a0289b598bdc
Aaron Angle <acangle10@gmail.com>
2020-03-27 11:50:00 -0700

    add

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+
+    pwa: {
+        name: 'Gila Hank',
+        themeColor: '#330066',
+        msTileColor: '#000000',
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: 'black',
+
+        // configure the manifest options
+        manifestOptions: {
+            background_color: "#f9fafd"
+        }
+    }
+}
